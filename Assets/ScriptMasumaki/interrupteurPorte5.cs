dca5c17f6d8bb3d38add4357c5f9ba6f7e06368a
Masumaki974 <mickael.focksiming@gmail.com>
2021-02-20 15:51:04 +0100

    Test3 Anim Porte

---
+++ Assets/ScriptMasumaki/interrupteurPorte5.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interrupteurPorte5 : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("sangoku", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("sangoku", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
