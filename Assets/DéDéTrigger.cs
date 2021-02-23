ce49c5513567df825957a3bc8f4b012b124ab831
Thomas DéDé <thomas.dedde@outlook.fr>
2021-02-23 19:12:57 +0100

    Test Door DéDé

---
+++ Assets/DéDéTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DéDéTrigger : MonoBehaviour
+{
+    public Animator animator;
+
+    
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("OpeningOrClosing", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("OpeningOrClosing", false);
+        }
+    }
+}
