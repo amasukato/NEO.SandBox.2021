720e07841a90d8ff1d84a5e66e49171a93794ee6
Zhelho (GuiBo) <44814654+Zelodix@users.noreply.github.com>
2021-02-23 15:27:27 +0100

    Test Door

---
+++ Assets/Scripts/DoorTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTest : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("IsOpening", true);
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("IsOpening", false);
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+
