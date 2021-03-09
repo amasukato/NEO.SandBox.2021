a98133aace0d8dab4e911237b31049abff90e41e
Zhelho <44814654+Zelodix@users.noreply.github.com>
2021-03-09 14:04:14 +0100

    UI folder

---
+++ Assets/UI/ManaBarRaw.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ManaBarRaw : MonoBehaviour {
+    
+    private RawImage RawManaBar;
+
+    private void Awake()
+    {
+        RawManaBar = transform.Find("RawManaBar").GetComponent<RawImage>();
+
+    }
+
+
+    
+
+
+
+   
+    
+}
